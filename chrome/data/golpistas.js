c47af68cf915ad8773f3d239260b8b845f9bc0c8
tortoyoyo <tortoyoyo@gmail.com>
2016-09-02 08:39:18 -0300

    ajuste de files

---
+++ chrome/data/golpistas.js
@@ -0,0 +1,34 @@
+window.addEventListener("load", init, false);
+function init() {
+    var retorno = document.body.innerHTML;
+    retorno = replaceListWordWithEnd(retorno, listSenadoresGolpistas, " Golpista", false);
+    retorno = replaceListWordWithEnd(retorno, listDeputadosGolpistas, " Golpista", false);
+    retorno = replaceListWordWithEnd(retorno, listPartidoGolpista, " Antidemocrático", true);
+    retorno = replaceListWord(retorno, listWord, false);
+    document.body.innerHTML = retorno;
+}
+
+function replaceListWordWithEnd(text, list, final, equal) {
+    for (var i = 0; i < list.length; i++) {
+        text = replaceWord(text, list[i], list[i] + final, equal);
+    }
+    return text;
+}
+
+function replaceListWord(text, list, equal) {
+    for (var i = 0; i < list.length; i++) {
+        text = replaceWord(text, list[i][0], list[i][1], equal);
+    }
+    return text;
+}
+
+function replaceWord(list, word, replace, equal) {
+    var re = null;
+    if (equal) {
+        re = new RegExp("\\b" + word + "\\b", "g");
+    } else {
+        re = new RegExp(word, "gi");
+    }
+    list = list.replace(re, replace);
+    return list;
+}
---
+++ firefox/data/outrasfrases.js
@@ -0,0 +1,7 @@
+var listWord = [
+    ['Dilma', 'Dilmãe'],
+    ['Michel Temer', 'Temer Golpista'],
+    ['Eduardo Cunha', 'Cunha Ladrão'],
+    ['impeachment', 'GOLPE'],
+    ['Primeiramente', 'Primeiramente fora Temer']
+];
