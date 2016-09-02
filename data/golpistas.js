80c7df7e7425bd2cf864333862d5bc1d26e87203
tortoyoyo <tortoyoyo@gmail.com>
2016-09-02 08:31:54 -0300

    ajustado bug dos partidos

--- data/golpistas.js
+++ data/golpistas.js
@@ -1,31 +1,35 @@
 window.addEventListener("DOMContentLoaded", init, false);
 
 function init() {
-console.log(listDeputadosGolpistas);
     var retorno = document.body.innerHTML;
-    retorno = replaceListWordWithEnd(retorno, listSenadoresGolpistas, " Golpista");
-    retorno = replaceListWordWithEnd(retorno, listDeputadosGolpistas, " Golpista");
-    // retorno = replaceListWordWithEnd(retorno, listPartidoGolpista, " Antidemocrático");
-    retorno = replaceListWord(retorno, listWord);
+    retorno = replaceListWordWithEnd(retorno, listSenadoresGolpistas, " Golpista", false);
+    retorno = replaceListWordWithEnd(retorno, listDeputadosGolpistas, " Golpista", false);
+    retorno = replaceListWordWithEnd(retorno, listPartidoGolpista, " Antidemocrático", true);
+    retorno = replaceListWord(retorno, listWord, false);
     document.body.innerHTML = retorno;
 }
 
-function replaceListWordWithEnd(text, list, final) {
+function replaceListWordWithEnd(text, list, final, equal) {
     for (var i = 0; i < list.length; i++) {
-        text = replaceWord(text, list[i], list[i] + final);
+        text = replaceWord(text, list[i], list[i] + final, equal);
     }
     return text;
 }
 
-function replaceListWord(text, list) {
+function replaceListWord(text, list, equal) {
     for (var i = 0; i < list.length; i++) {
-        text = replaceWord(text, list[i][0], list[i][1]);
+        text = replaceWord(text, list[i][0], list[i][1], equal);
     }
     return text;
 }
 
-function replaceWord(list, word, replace) {
-    var re = new RegExp(word, "gi");
+function replaceWord(list, word, replace, equal) {
+    var re = null;
+    if (equal) {
+        re = new RegExp("\\b" + word + "\\b", "g");
+    } else {
+        re = new RegExp(word, "gi");
+    }
     list = list.replace(re, replace);
     return list;
 }
